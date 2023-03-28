d82204b68205932d17aecf467dcfb7babbda4fc3
bimaagung <bimaagung.labs@gmail.com>
2023-03-28 19:19:50 +0700

    done test case the birthday bar

--- thebirthdaybar/thebirtdaybar_test.go
+++ thebirthdaybar/thebirtdaybar_test.go
@@ -22,13 +22,78 @@ Determine how many ways she can divide the chocolate.
 
 PROGRAM theBirthdayBar
 
+INPUT
+var squareChoco: array
+var day, month
+
+DEKLARASI
+result: interger
+sumResult: integer
+
+ALGORITMA
+IF S[0] equal day
+	OUTPUT 1
+ENDIF
+
+FOR
+	sumResult = 0
+
+	IF long from squareChoco month
+		BREAK
+	ENDIF
+
+	FOR i equal 0 to month itterate ++
+		sumResult =+ squareChoco[i]
+	ENDFOR
+
+	IF sumResult equals day
+		result++
+	ENDIF
+
+	squareChoco = array of squareChoco always slice 1 item from start
+
+	CONTINUE
+
+ENDFOR
+
+
+OUTPUT result
+
 
 END
 **/
 
 
 func theBirthdayBar(s []int32, d, m int32) int32 {
-	return 0
+
+	var result int32 = 0
+
+	if s[0] == d {
+		return 1
+	}
+
+	for {
+		var sumResult int32 = 0
+
+		if int32(len(s)) < m {
+			break
+		}
+
+		for i := int32(0); i < m; i++ {
+			sumResult += s[i]
+		}
+
+		if sumResult == d {
+			result++
+		}
+
+		s = s[1:]
+
+		continue
+	}
+
+
+	return result
 }
 
 
@@ -50,6 +115,9 @@ func TestTheBirthdayBar(t *testing.T){
 		{
 			[]int32{4},4,1,1,
 		},
+		{
+			[]int32{2,5,1,3,4,4,3,5,1,1,2,1,4,1,3,3,4,2,1}, 18, 7, 3,
+		},
 	}
 
 	for _, v := range testCase {
@@ -57,7 +125,8 @@ func TestTheBirthdayBar(t *testing.T){
 		
 		t.Run(testName, func(t *testing.T) {
 			result := theBirthdayBar(v.s, v.d, v.m)
-			assert.Equal(t, result, v.output)
+
+			assert.Equal(t, v.output, result)
 		})
 	}
 }
